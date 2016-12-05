880227af10d6ce03056d5b0bc55b3e897869f46d
Yobleed <freelauncher1@hotmail.com>
2016-12-05 11:01:33 +0100

    Add files via upload

--- fakeunits.lua
+++ fakeunits.lua
@@ -1,9 +1,9 @@
 local LibDispellable = LibStub('LibDispellable-1.0')
 
-NeP.FakeUnits:Add('canDispell', function(spell)
+NeP.FakeUnits:Add('CanDispell', function(spell)
     local tempTable = {}
     for _, Obj in pairs(NeP.OM:GetRoster()) do
-      if LibDispellable:CanDispelWith(Obj.key, 527) then
+      if LibDispellable:CanDispelWith(Obj.key, NeP.Core:GetSpellID(spell)) then
         tempTable[#tempTable+1] = {
                 key = Obj.key,
                 health = Obj.health
